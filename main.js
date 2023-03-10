00175f801685b3f55abdff4d7416cdff9d04ab76
Amin Pak <amin.pak008@gmail.com>
2023-03-10 11:30:36 -0800

    Add car 3D file

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-// Create a scene
-const scene = new THREE.Scene();
+// Define the scene, camera, and renderer
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// Create a camera
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Define the material for the car body
+var bodyMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 
-// Create a renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// Load the car model
+var loader = new THREE.GLTFLoader();
+loader.load('car.glb', function (gltf) {
+  // Add the car to the scene
+  var car = gltf.scene;
+  car.scale.set(0.1, 0.1, 0.1);
+  car.position.set(0, -1, 0);
+  scene.add(car);
 
-// Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  // Set the material for the car body
+  car.traverse(function (node) {
+    if (node.isMesh) node.material = bodyMaterial;
+  });
+});
 
-// Position the camera
-camera.position.z = 5;
+// Add some lights to the scene
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-// Render the scene
+var pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.set(5, 5, 5);
+camera.add(pointLight);
+scene.add(camera);
+
+// Animate the scene
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 animate();
